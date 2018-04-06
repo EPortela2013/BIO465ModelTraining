d52825df44059049bb2e67850c78c195bd4290d1
Edward Andrey Portela Portela <Eportela2007@cfl.rr.com>
2018-04-06 06:02:35 -0400

    Update train.sh

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@
 CAFFE_ROOT=/opt/caffe/distribute/bin
 
 echo STARTING AT `date`
-
-$CAFFE_ROOT/caffe train -solver solver.prototxt -weights models/bvlc_googlenet.caffemodel -gpu all &> caffe.log
+# if caffe compiled with GPU mode enabled, add -gpu all as argument
+$CAFFE_ROOT/caffe train -solver solver.prototxt -weights models/bvlc_googlenet.caffemodel &> caffe.log
 
 echo FINISHED at `date`
