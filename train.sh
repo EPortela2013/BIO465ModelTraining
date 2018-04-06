a6e7a79b57194a730cf2cbd85952b271b78be6c9
Edward Andrey Portela Portela <Eportela2007@cfl.rr.com>
2018-04-06 05:33:51 -0400

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+CAFFE_ROOT=/opt/caffe/distribute/bin
 
 echo STARTING AT `date`
 
-caffe train -solver solver.prototxt -weights models/bvlc_googlenet.caffemodel -gpu all &> caffe.log
+$CAFFE_ROOT/caffe train -solver solver.prototxt -weights models/bvlc_googlenet.caffemodel -gpu all &> caffe.log
 
 echo FINISHED at `date`
